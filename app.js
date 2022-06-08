387b232eb6a55a61876a0bd81373beb6d1c808ad
Gameover0516 <105245448+Gameover0516@users.noreply.github.com>
2022-06-08 00:17:13 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const arregloEstudiantes = [
+    new Estudiante("Alam Jair", "Ramirez Velazquez", "Informatica"),
+    new Estudiante("Valeria del Carmen","Perez Contreras","Informatica"),
+    new Estudiante("Sofia Natalia", "Echeverria", "Gestion Empresarial")
+]
+function mostrarEstudiantes(){
+    let texto=""
+    for(let item of arregloEstudiantes){
+        texto += `<li>${item.nombre} ${item.apellido} ${item.carrera} </li>`
+    }
+    document.getElementById("alumnos").innerHTML = texto
+}
+function arregloEstudiante(){
+    const formulario = document.forms["formulario"]
+    const nombre = formulario["nombre"]
+    const apellido = formulario["apellido"]
+    const carrera = formulario["carrera"]
+    const nvoEstudiante = new Estudiante(nombre.value,apellido.value,carrera.value)
+    arregloEstudiantes.push(nvoEstudiante)
+    mostrarEstudiantes()
+}
