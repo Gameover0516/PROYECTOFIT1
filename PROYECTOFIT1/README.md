d1cef3963b1cc8425468b933188e3ff99b8ea1b5
Gameover0516 <105245448+Gameover0516@users.noreply.github.com>
2022-06-18 12:25:31 -0500

    Add files via upload

---
+++ PROYECTOFIT1/README.md
@@ -0,0 +1,1 @@
+# PROYECTOFIT1
---
+++ PROYECTOFIT1/java/estuidante.js
@@ -0,0 +1,25 @@
+class Estudiante  {
+    constructor(nombre, apellido, carrera){
+        this._nombre=nombre
+        this._apellido=apellido
+        this._carrerra=carrera
+    }
+    get nombre(){
+        return this._nombre
+    }
+    set nombre(nombre){
+        this._nombre = nombre
+    }
+    get apellido(){
+        return this._apellido
+    }
+    set apellido(apellido){
+        this._apellido = apellido
+    }
+    get carrera(){
+        return this._carrerra
+    }
+    set carrera(carrera){
+        this._carrerra = carrera
+    }
+}
